74cec82313298b7bf4936d6490d98e296b3c968c
vijaym105 <malusarevijay10@gmail.com>
2025-11-14 19:23:08 +0530

    JS assignment 1

---
+++ JS/JS assign 1/script.js
@@ -0,0 +1,61 @@
+
+//  Logging and interaction
+// console.log("HEy")
+// console.info("Tags");
+// console.warn("Tags");
+// console.error("Tags");
+// const vj = prompt("Whats your name");
+// alert("Welcome",`${vj}`);
+// let age = Number(prompt("Whats's your age?"));
+// console.log(typeof age);
+// let age = prompt("Enter age");
+// console.log(age + 5); // Happens cause age is a string , resultant concatenation happens
+
+
+// Working with Strings
+// let a = "I love Sheriyans";
+// a.split(2,6);
+// a.split("");
+// a.replace("love", "at")
+// console.log(a);
+// a.includes("love");
+// let nam = "Vijay";
+// console.log("Hey" , `${nam}`);
+
+
+// Statements
+// let a = 4; b=3;
+// console.log(a)
+// console.log(a)
+// console.log(a)
+
+//Expressions
+// let x = (5+10) *2;
+// console.log(x);
+
+
+//DataTypes
+// let nam = "VIjay"; String
+// let age =  12; number/integer
+// let cgpa = 13.4; float
+// let isStudent = true; boolean
+// let back = null; null
+// let skill = ["JS","HTMl","CSS"]; array
+// let city = {city:"pune"};  object
+// let y; undefined
+
+
+// Special values
+// let nan = Number(12+"bs");
+// console.log(nan); // NaN (Not a Number)
+// let logs =  1/0;
+// console.log(logs); // Infinity
+// Write one line explaining when to use null vs undefined. > null is used intentional absence , undefined for unintentional absence.
+
+// Primitive vs Reference
+let x  = 5;
+let y = x;
+y = 10;
+console.log(x,y);
+
+
